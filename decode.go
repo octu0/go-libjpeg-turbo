9086b6f135b274b20b55400585f84cf6bc0589a4
Yusuke Hata <yusuke.hata@mirrativ.co.jp>
2022-11-02 13:25:08 +0900

    add DecodeToRGBA

--- decode.go
+++ decode.go
@@ -12,11 +12,21 @@ import "C"
 
 import (
 	"bytes"
+	"image"
 	"runtime"
+	"sync"
 	"sync/atomic"
 	"unsafe"
 
 	"github.com/pkg/errors"
+)
+
+var (
+	imageBufPool = &sync.Pool{
+		New: func() interface{} {
+			return bytes.NewBuffer(make([]byte, 0, 16*1024))
+		},
+	}
 )
 
 type Header struct {
@@ -45,8 +55,20 @@ func (i *Image) Close() {
 	}
 }
 
-func finalizeImage(i *Image) {
-	i.Close()
+type ImageRGBARef struct {
+	Header
+	Format PixelFormat
+	Image  *image.RGBA
+	buf    *bytes.Buffer
+	closed int32
+}
+
+func (i *ImageRGBARef) Close() {
+	if atomic.CompareAndSwapInt32(&i.closed, 0, 1) {
+		runtime.SetFinalizer(i, nil)
+
+		imageBufPool.Put(i.buf)
+	}
 }
 
 type Decoder struct {
@@ -74,7 +96,7 @@ func (d *Decoder) DecodeHeader(data []byte) (Header, error) {
 	}, nil
 }
 
-func (d *Decoder) Decode(data []byte, format PixelFormat) (*Image, error) {
+func (d *Decoder) decode(out *bytes.Buffer, data []byte, format PixelFormat) (Header, error) {
 	r := unsafe.Pointer(C.decode_jpeg(
 		(C.tjhandle)(d.handle),
 		(*C.uchar)(unsafe.Pointer(&data[0])),
@@ -82,29 +104,66 @@ func (d *Decoder) Decode(data []byte, format PixelFormat) (*Image, error) {
 		C.int(format),
 	))
 	if r == nil {
-		return nil, errors.Errorf("failed to call tjDecompress2()")
+		return Header{}, errors.Errorf("failed to call tjDecompress2()")
 	}
 	result := (*C.jpeg_decode_result_t)(r)
 	defer C.free_jpeg_decode_result(result)
 
-	buf := imageBufPool.Get().(*bytes.Buffer)
-	buf.Reset()
-	buf.Grow(int(result.data_size))
-	buf.Write(C.GoBytes(unsafe.Pointer(result.data), result.data_size)) // copy C allocated
+	out.Reset()
+	out.Grow(int(result.data_size))
+	out.Write(C.GoBytes(unsafe.Pointer(result.data), result.data_size)) // copy C allocated
+
+	return Header{
+		Width:       int(result.width),
+		Height:      int(result.height),
+		Subsampling: Subsampling(result.subsampling),
+		ColorSpace:  ColorSpace(result.colorspace),
+	}, nil
+}
+
+func (d *Decoder) Decode(data []byte, format PixelFormat) (*Image, error) {
+	out := imageBufPool.Get().(*bytes.Buffer)
+	header, err := d.decode(out, data, format)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
 
 	img := &Image{
-		Header: Header{
-			Width:       int(result.width),
-			Height:      int(result.height),
-			Subsampling: Subsampling(result.subsampling),
-			ColorSpace:  ColorSpace(result.colorspace),
-		},
-		Format: PixelFormat(result.pixel_format),
-		buf:    buf,
+		Header: header,
+		Format: format,
+		buf:    out,
 		closed: 0,
 	}
-	runtime.SetFinalizer(img, finalizeImage)
+	runtime.SetFinalizer(img, func(me *Image) {
+		me.Close()
+	})
 	return img, nil
+}
+
+func (d *Decoder) DecodeToRGBA(data []byte) (*ImageRGBARef, error) {
+	out := imageBufPool.Get().(*bytes.Buffer)
+	out.Reset()
+	header, err := d.decode(out, data, PixelFormatRGBA)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	rgba := &image.RGBA{
+		Pix:    out.Bytes(),
+		Stride: header.Width * 4,
+		Rect:   image.Rect(0, 0, header.Width, header.Height),
+	}
+	ref := &ImageRGBARef{
+		Header: header,
+		Format: PixelFormatRGBA,
+		Image:  rgba,
+		buf:    out,
+		closed: 0,
+	}
+	runtime.SetFinalizer(ref, func(me *ImageRGBARef) {
+		me.Close()
+	})
+	return ref, nil
 }
 
 func (d *Decoder) Close() {
